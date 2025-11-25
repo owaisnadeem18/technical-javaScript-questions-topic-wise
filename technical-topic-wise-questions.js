6bc783aebd883eae09690ac709cb36b602888540
Owais Nadeem <108211200+owaisnadeem18@users.noreply.github.com>
2025-11-25 20:52:29 +0500

    advance JS questions

--- technical-topic-wise-questions.js
+++ technical-topic-wise-questions.js
@@ -19,7 +19,39 @@
 // 2. Hoisted but remains in the TDZ until the declaration is executed. Must be initialized at the time of declaration.
 // 3. Cannot reassign nor redeclare.
 
+// ⭐ null
+// ⭐ undefined
+// ⭐ null vs undefined
 
-console.log(a)
+// null: It is an assignment value. It can be assigned to a variable as a representation of no value. Type of null is 'object'.
 
-let a = 34 
+// undefined: It means a variable has been declared but has not yet been assigned a value. Type of undefined is 'undefined'.    
+
+// null vs undefined:
+
+// 1. null is an assignment value, whereas undefined means a variable has been declared but not assigned a value.
+// 2. Type of null is 'object', whereas type of undefined is 'undefined'.
+// 3. null must be explicitly assigned to a variable, whereas undefined is the default value for uninitialized variables.
+
+// Example:
+// let user1;
+// console.log(user1); // undefined (JS default, NOT null)
+
+// let user2 = null;
+// console.log(user2); // null (explicitly assigned)
+
+// Type of Null :
+
+// console.log(typeof null)
+// Output: Object
+
+// NaN 
+
+// Type of NaN :
+// console.log(typeof NaN)
+// Output: number 
+
+// let greet = "Hello"
+
+// console.log(greet * 45 ) // Output: NaN 
+
