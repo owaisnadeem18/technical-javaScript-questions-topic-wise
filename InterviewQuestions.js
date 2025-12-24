27a913b6e0ef47b9cad30b104c21d907b0783893
Owais Nadeem <108211200+owaisnadeem18@users.noreply.github.com>
2025-12-24 20:27:18 +0500

    advanced JavaScript questions

---
+++ InterviewQuestions.js
@@ -0,0 +1,64 @@
+// Technical and advance questions of JavaScript:
+
+// 1. 
+// 🔁 EVENT LOOP / ASYNC (VERY IMPORTANT)
+
+// console.log("A");
+
+// setTimeout(() => {
+//   console.log("B");
+// }, 0);
+
+// Promise.resolve().then(() => {
+//   console.log("C");                                                                                                                                                  
+// });
+                                      
+// console.log("D");
+       
+// Output:
+// A          
+// D
+// C  
+// B
+
+// swap the values of two numbers without creating a new variable:
+
+// let a = 32 
+// let b = 24
+
+// console.log("The value of a & b after swap is => ", a  , b)
+
+// a = b + a 
+
+// b = a - b
+
+// // Now , we wanna swap the value of "a"
+
+// a = a - b 
+
+// console.log("The value of a & b after swap is => ", a  , b)
+
+// ------------------ 
+
+console.log("start");
+
+setTimeout(() => {
+  console.log("timeout");
+}, 0);
+
+Promise.resolve()
+  .then(() => {
+    console.log("promise-1");
+  })
+  .then(() => {
+    console.log("promise-2");
+  });
+
+console.log("end");
+
+// Output: 
+// start
+// end
+// promise-1
+// promise-2
+// timeout
