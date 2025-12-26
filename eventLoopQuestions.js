26a0adb23f7d8cfd53a329ef9c45c7d055024bc4
Owais Nadeem <108211200+owaisnadeem18@users.noreply.github.com>
2025-12-26 23:56:11 +0500

    event loop questions

--- eventLoopQuestions.js
+++ eventLoopQuestions.js
@@ -20,3 +20,56 @@
 // Timeout 1
 // Timeout 2
 
+// ------------------------------------------ 
+
+// async function test() {
+//     console.log('A'); 
+//     await Promise.resolve();
+//     console.log('B');
+// }
+
+// test();
+// console.log('C');
+
+// Output: 
+// A 
+// C 
+// B 
+
+
+// ------------------------------------------ 
+
+// 2. Multiple microtasks
+
+// Promise.resolve().then(() => console.log('P1'));
+// queueMicrotask(() => console.log('QM1'));
+// Promise.resolve().then(() => console.log('P2'));
+
+// console.log('Sync');
+
+// Output: 
+
+// Sync
+// P1
+// QM1
+// P2
+
+// ------------------------------------------ 
+
+// 4. Nested setTimeout + async
+
+setTimeout(async () => {
+    console.log('T1 start');
+    await Promise.resolve();
+    console.log('T1 end');
+}, 0);
+
+setTimeout(() => console.log('T2'), 0);
+console.log('Sync');
+
+// Output:
+ 
+// Sync
+// T1 start
+// T1 end
+// T2        
